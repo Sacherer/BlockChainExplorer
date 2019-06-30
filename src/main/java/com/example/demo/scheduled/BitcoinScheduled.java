bdcf3f5b50789a6cc4d0e069f8a3bbe59581c4c9
Sacher <Sacherer@163.com>
2019-06-30 10:54:12 +0800

    finish explorer

--- src/main/java/com/example/demo/scheduled/BitcoinScheduled.java
+++ src/main/java/com/example/demo/scheduled/BitcoinScheduled.java
@@ -5,6 +5,7 @@ import com.example.demo.service.TransationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -23,19 +24,22 @@ public class BitcoinScheduled {
     @Autowired
     private TransationService transationService;
 
+    @Value("${bitcoin.sync.startHash}")
+    private String blockHash;
+
     @Scheduled(cron = "${bitcoin.sync.cron}")
     public void setScheduled(){
         logger.info("the scheduled is start");
-        bitconinSyncService.syncBlockChain("000000000000030d7d5d9c86cb604350170a946de104203aeaba033e98167c04");
+        bitconinSyncService.syncBlockChain(blockHash);
     }
     @Scheduled(cron = "${bitcoin.txIndex.cron}")
     public void txIndex(){
-        logger.info("begin to send data {}",transationService.getTxIndex());
+//        logger.info("begin to send data {}",transationService.getTxIndex());
         simpMessagingTemplate.convertAndSend("/topic/txIndex",transationService.getTxIndex());
     }
     @Scheduled(cron = "${bitcoin.txMore.cron}")
     public void txMore(){
-        logger.info("begin to send data {}",transationService.getMoreTx());
+//        logger.info("begin to send data {}",transationService.getMoreTx());
         simpMessagingTemplate.convertAndSend("/topic/txMore",transationService.getMoreTx());
     }
 }
